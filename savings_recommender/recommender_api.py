46072237896367e8caf993e43164912d77e0a0da
Kari <kdavis7190@gmail.com>
2019-04-08 12:19:29 -0700

    add_proj_5

---
+++ savings_recommender/recommender_api.py
@@ -0,0 +1,354 @@
+from flask import Flask, send_file, render_template, request, jsonify
+import pickle
+import numpy as np
+import pandas as pd
+import math
+from math import radians, cos, sin, asin, sqrt
+import re
+from sklearn.metrics.pairwise import cosine_similarity
+from collections import defaultdict
+
+from bokeh.io import output_file, show
+from bokeh.embed import components, file_html
+from bokeh.models import CategoricalColorMapper, HoverTool, Legend, Range1d
+from bokeh.core.properties import value
+from bokeh.palettes import Category20c
+from bokeh.plotting import figure, output_file, show
+from bokeh.transform import cumsum
+from bokeh.resources import CDN
+
+def clean_statement(csv):
+
+    transactions = pd.read_csv(csv)
+    transactions.dropna()
+    transactions = transactions.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    transactions = transactions[transactions['Address'].str.lower() == 'san francisco ca']
+
+    transactions['Payee'] = transactions['Payee'].str.lower()
+    transactions['Payee'] = transactions['Payee'].str.replace('san franciscoca', '')
+
+    businesses = []
+
+    for business in transactions['Payee']:
+        if '*' in business:
+            business = business.split('*')[1]
+        if '-' in business:
+            business = business.split('-')[0]
+        if '#' in business:
+            business = business.split('#')[0]
+        if '.' in business:
+            business = business.split('.')[0]
+        if '(' in business:
+            business = business.split('(')[0]
+
+        regex = re.compile(r'[0-9]{2,}')
+        if regex.search(business):
+            business = re.sub(regex, '', business)
+
+        city_stopwords = ['potrero', 'fillmore', 'deli', 'whse', 'llc', 'tru', 'lndry', 'union sq', 'sfo', 'usa',
+                          'marina']
+
+        for stopword in city_stopwords:
+            if stopword in business:
+                business = business.replace(stopword, '')
+
+        businesses.append(business.strip())
+
+        spendings = {}
+
+        for place in businesses:
+            matches = transactions[transactions['Payee'].str.contains(place, case=False)]
+            matches = matches.reset_index()
+            purchases = []
+            for idx in range(0, len(matches)):
+                month = matches['Posted Date'][idx][:2]
+                amt = (matches['Amount'][idx])*(-1)
+                purchases.append((month, amt))
+            spendings[place] = purchases
+
+    return businesses, spendings
+
+
+def best_business_recs(business, matrix, df):
+
+    # calulate cosine similarity between all rows in matrix
+    cosine_sim = cosine_similarity(matrix, matrix)
+
+    # locate business of interest and sort all cosine_sim's with that row
+    idx = df.loc[df.business == business].index[0]
+    scores = cosine_sim[idx,:]
+    ordered = scores.argsort()[::-1]
+
+    matches = []
+    match_locations = []
+    categories = []
+    bus_lat = df['lat'][idx]
+    bus_lng = df['lng'][idx]
+
+    for col in df.columns:
+        if df[col][idx] == 1:
+            categories.append(col)
+
+    # count all matching business categories and absolute lat/lng distance
+    for similar_business in ordered[1:26]:
+        count = 0
+        for category in categories:
+            if df[category][similar_business] == 1:
+                count += 1
+
+        match_lat = df['lat'][similar_business]
+        match_lng = df['lng'][similar_business]
+
+        distance = abs(haversine(bus_lng, bus_lat, match_lng, match_lat))
+
+        match_locations.append((similar_business, distance))
+        matches.append((similar_business, count))
+
+    sorted_category_matches = sorted(matches, key=lambda x: x[1], reverse=True)
+    sorted_location_matches = sorted(match_locations, key=lambda x: x[1])
+
+    weighted_matches = []
+
+    # weight each rec on their order of appearance in cosine_sim, categories, and distance
+    for item in ordered[1:26]:
+        weight_cos = np.where(ordered==item)[0]
+        weight_cat = [index for index, tup in enumerate(sorted_category_matches) if tup[0] == item][0]
+        weight_loc = [index for index, tup in enumerate(sorted_location_matches) if tup[0] == item][0]
+
+        weight = weight_cos + weight_cat + weight_loc
+
+        weighted_matches.append((item, weight))
+
+    weighted_matches = sorted(weighted_matches, key=lambda x: x[1])
+
+    business_recs = []
+
+    for tup in weighted_matches[:15]:
+        business_recs.append(tup[0])
+
+    # find cheaper and better rated businesses
+    save_money_recs = {}
+    higher_rated_recs = {}
+
+    cheaper_places = []
+    higher_rated_places = []
+
+    for rec in business_recs:
+        if df['price'][rec] < df['price'][idx]:
+            name = df['business'][rec]
+            price = df['price'][rec]
+            rating = df['rating'][rec]
+            pic = df['image_url'][rec]
+            link = df['url'][rec]
+            category = df['business_type'][rec]
+            rec_dict = {"name": name, "price": price, "pic": pic, "link":link, "rating": rating, "category": category}
+            cheaper_places.append(rec_dict)
+            if df['rating'][rec] > df['rating'][idx]:
+                name = df['business'][rec]
+                price = df['price'][rec]
+                rating = df['rating'][rec]
+                pic = df['image_url'][rec]
+                link = df['url'][rec]
+                category = df['business_type'][rec]
+                higher_rated_dict = {"name": name, "price": price, "pic": pic, "link":link, "rating": rating, "category": category}
+                higher_rated_places.append(higher_rated_dict)
+
+    save_money_recs[df['business'][idx]] = cheaper_places
+    higher_rated_recs[df['business'][idx]] = higher_rated_places
+
+    final_recs = []
+
+    # get business names for return and remove repeats
+    for bus in business_recs:
+        rec = df['business'][bus]
+        if rec not in final_recs:
+            if rec != df['business'][idx]:
+                final_recs.append(rec)
+
+    return final_recs[:5], save_money_recs, higher_rated_recs
+
+
+def statement_to_recs(clean_statement_list, spendings, num_matrix, df):
+
+    all_monthly_recs = []
+    save_money_recs = {}
+    higher_rated_recs = {}
+    category_counts = defaultdict(int)
+    spendings_dict = {}
+
+    for place in clean_statement_list:
+
+        try:
+            matches = df[df['business'].str.contains(place, case=False)]
+
+            matches = matches.reset_index()
+            business_match = matches['business'][0]
+
+            for col in matches.columns:
+                if matches[col][0] == 1:
+                    category_counts[col] +=1
+
+            spent = spendings[place]
+            spendings_dict[business_match] = spent
+
+            all_recs, cheap_recs, best_recs = best_business_recs(business_match, num_matrix, df)
+
+            for rec in all_recs:
+                all_monthly_recs.append(rec)
+            for key, value in cheap_recs.items():
+                save_money_recs[key] = value
+            for key, value in best_recs.items():
+                higher_rated_recs[key] = value
+
+        except:
+            pass
+
+    return all_monthly_recs, save_money_recs, higher_rated_recs, category_counts, spendings_dict
+
+def spendings_plot(spendings_dict, df):
+
+    food12 = []
+    coffee12 = []
+    services12 = []
+    retail12 = []
+    bars12 = []
+
+    food01 = []
+    coffee01 = []
+    services01 = []
+    retail01 = []
+    bars01 = []
+
+    food02 = []
+    coffee02 = []
+    services02 = []
+    retail02 = []
+    bars02 = []
+
+    food03 = []
+    coffee03 = []
+    services03 = []
+    retail03 = []
+    bars03 = []
+
+    food = []
+    coffee = []
+    services = []
+    retail = []
+    bars = []
+
+    months = ['12', '01', '02', '03']
+
+    data = { 'Month': months }
+
+    for key, value in spendings_dict.items():
+        matches = df[df['business'].str.contains(key, case=False)]
+        matches = matches.reset_index()
+        category = matches['business_type'][0]
+
+        if category == 'food':
+            for amt in value:
+                if amt[0] == '12':
+                    food12.append(amt[1])
+                elif amt[0] == '01':
+                    food01.append(amt[1])
+                elif amt[0] == '02':
+                    food02.append(amt[1])
+                elif amt[0] == '03':
+                    food03.append(amt[1])
+        elif category == 'coffee':
+            for amt in value:
+                if amt[0] == '12':
+                    coffee12.append(amt[1])
+                elif amt[0] == '01':
+                    coffee01.append(amt[1])
+                elif amt[0] == '02':
+                    coffee02.append(amt[1])
+                elif amt[0] == '03':
+                    coffee03.append(amt[1])
+        elif category == 'services':
+            for amt in value:
+                if amt[0] == '12':
+                    services12.append(amt[1])
+                elif amt[0] == '01':
+                    services01.append(amt[1])
+                elif amt[0] == '02':
+                    services02.append(amt[1])
+                elif amt[0] == '03':
+                    services03.append(amt[1])
+        elif category == 'bars':
+            for amt in value:
+                if amt[0] == '12':
+                    bars12.append(amt[1])
+                elif amt[0] == '01':
+                    bars01.append(amt[1])
+                elif amt[0] == '02':
+                    bars02.append(amt[1])
+                elif amt[0] == '03':
+                    bars03.append(amt[1])
+        elif category == 'retail':
+            for amt in value:
+                if amt[0] == '12':
+                    retail12.append(amt[1])
+                elif amt[0] == '01':
+                    retail01.append(amt[1])
+                elif amt[0] == '02':
+                    retail02.append(amt[1])
+                elif amt[0] == '03':
+                    retail03.append(amt[1])
+
+    food.append(sum(food12))
+    food.append(sum(food01))
+    food.append(sum(food02))
+    food.append(sum(food03))
+
+    bars.append(sum(bars12))
+    bars.append(sum(bars01))
+    bars.append(sum(bars02))
+    bars.append(sum(bars03))
+
+    coffee.append(sum(coffee12))
+    coffee.append(sum(coffee01))
+    coffee.append(sum(coffee02))
+    coffee.append(sum(coffee03))
+
+    retail.append(sum(retail12))
+    retail.append(sum(retail01))
+    retail.append(sum(retail02))
+    retail.append(sum(retail03))
+
+    services.append(sum(services12))
+    services.append(sum(services01))
+    services.append(sum(services02))
+    services.append(sum(services03))
+
+    colors = ['#78aac3', '#f07bbb', '#658138', '#dab48d', '#c8d5f6']
+    legends_list = ['Food', 'Coffee', 'Retail', 'Services', 'Bars']
+    ys=[food, coffee, retail, services, bars]
+    xs=[months, months, months, months, months]
+
+    p = figure(x_range=months, plot_width=700, plot_height=500,title="Monthly Spending Per Category", x_axis_label="Month", y_axis_label="Amount Spent")
+
+    for (colr, leg, x, y ) in zip(colors, legends_list, xs, ys):
+        p.line(x, y, color= colr, line_width=4, legend= leg)
+
+    p.legend.location = "center_right"
+
+
+    script, div = components(p)
+
+    return script, div, food, services, bars, coffee, retail
+
+
+def haversine(lng1, lat1, lng2, lat2):
+
+    # convert decimal degrees to radians
+    lng1, lat1, lng2, lat2 = map(radians, [lng1, lat1, lng2, lat2])
+
+    # haversine formula
+    dlng = lng2 - lng1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlng/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 3956 # Radius of earth in miles
+    return c * r
---
+++ savings_recommender/recommender_app.py
@@ -0,0 +1,265 @@
+from flask import Flask, send_file, render_template, request, jsonify, flash, redirect, url_for, send_from_directory
+from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
+from recommender_api import clean_statement, best_business_recs, statement_to_recs, spendings_plot, haversine
+from math import radians, cos, sin, asin, sqrt
+
+from bokeh.io import output_file, show
+from bokeh.embed import components, file_html
+from bokeh.models import CategoricalColorMapper, HoverTool, Legend, Range1d
+from bokeh.core.properties import value
+from bokeh.palettes import Category20c
+from bokeh.plotting import figure, output_file, show
+from bokeh.transform import cumsum
+from bokeh.resources import CDN
+
+import pickle
+import numpy as np
+import pandas as pd
+import os
+import re
+from sklearn.metrics.pairwise import cosine_similarity
+from collections import defaultdict
+
+UPLOAD_FOLDER = '/Users/kari/ds/metis/metisgh/kojak/flask_app'
+ALLOWED_EXTENSIONS = set(['csv'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+Bootstrap(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('Incorrect File Extension')
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No File Entered')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template("homepage.html")
+
+
+@app.route('/api/uploads/<filename>')
+def uploaded_file(filename):
+    send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    return redirect(url_for('user_recs', filename=filename))
+
+
+@app.route('/recs/<filename>', methods=["POST", "GET"])
+def user_recs(filename):
+
+    num_matrix = pd.read_pickle("./num_matrix.pkl")
+    df = pd.read_pickle('./final_df.pkl')
+
+    statement = '/Users/kari/ds/metis/metisgh/kojak/flask_app/' + filename
+    cleaned_statement, spendings = clean_statement(statement)
+    all_recs, cheap_recs, high_rates_recs, category_counts, spendings_dict = statement_to_recs(cleaned_statement, spendings, num_matrix, df)
+
+    no_matches = []
+    for key, value in cheap_recs.items():
+        if len(value) < 1:
+            no_matches.append(key)
+
+    for key in no_matches:
+        del cheap_recs[key]
+
+    no_matches_rate = []
+    for key, value in high_rates_recs.items():
+        if len(value) < 1:
+            no_matches_rate.append(key)
+
+    for key in no_matches_rate:
+        del high_rates_recs[key]
+
+    rec_breakdown = {}
+    category_total = 0
+    for key, value in category_counts.items():
+        category_total = category_total + value
+    for key, value in category_counts.items():
+        rec_breakdown[key] = value/category_total
+
+    food = {}
+    coffee = {}
+    bars = {}
+    services = {}
+    retail = {}
+
+    for key, value in spendings_dict.items():
+        matches = df[df['business'].str.contains(key, case=False)]
+        matches = matches.reset_index()
+        category = matches['business_type'][0]
+
+        if category == 'food':
+            total_spent = 0
+            for amt in value:
+                total_spent = total_spent + amt[1]
+            food[key] = total_spent
+        elif category == 'coffee':
+            total_spent = 0
+            for amt in value:
+                total_spent = total_spent + amt[1]
+            coffee[key] = total_spent
+        elif category == 'retail':
+            total_spent = 0
+            for amt in value:
+                total_spent = total_spent + amt[1]
+            retail[key] = total_spent
+        elif category == 'services':
+            total_spent = 0
+            for amt in value:
+                total_spent = total_spent + amt[1]
+            services[key] = total_spent
+        elif category == 'bars':
+            total_spent = 0
+            for amt in value:
+                total_spent = total_spent + amt[1]
+            bars[key] = total_spent
+
+    coffee_visited_tiers = []
+    food_visited_tiers = []
+    services_used_tiers = []
+    bars_visited_tiers = []
+    retail_visited_tiers = []
+
+    for key, value in coffee.items():
+        matches = df[df['business'].str.contains(key, case=False)]
+        matches = matches.reset_index()
+        price = matches['price'][0]
+        coffee_visited_tiers.append(price)
+
+    for key, value in food.items():
+        matches = df[df['business'].str.contains(key, case=False)]
+        matches = matches.reset_index()
+        price = matches['price'][0]
+        food_visited_tiers.append(price)
+
+    for key, value in bars.items():
+        matches = df[df['business'].str.contains(key, case=False)]
+        matches = matches.reset_index()
+        price = matches['price'][0]
+        bars_visited_tiers.append(price)
+
+    for key, value in services.items():
+        matches = df[df['business'].str.contains(key, case=False)]
+        matches = matches.reset_index()
+        price = matches['price'][0]
+        services_used_tiers.append(price)
+
+    for key, value in retail.items():
+        matches = df[df['business'].str.contains(key, case=False)]
+        matches = matches.reset_index()
+        price = matches['price'][0]
+        retail_visited_tiers.append(price)
+
+    coffee_recs_tiers = []
+    food_recs_tiers = []
+    services_recs_tiers = []
+    bars_recs_tiers = []
+    retail_recs_tiers = []
+
+    for key, value in high_rates_recs.items():
+        for rec in value:
+            type = rec['category']
+            if type == "coffee":
+                coffee_recs_tiers.append(rec['price'])
+            elif type == "food":
+                food_recs_tiers.append(rec['price'])
+            elif type == "retail":
+                retail_recs_tiers.append(rec['price'])
+            elif type == "services":
+                services_recs_tiers.append(rec['price'])
+            elif type == "bars":
+                bars_recs_tiers.append(rec['price'])
+
+    coffee_spent_avg = sum(coffee_visited_tiers)/len(coffee_visited_tiers)
+    food_spent_avg = sum(food_visited_tiers)/len(food_visited_tiers)
+    bars_spent_avg = sum(bars_visited_tiers)/len(bars_visited_tiers)
+    retail_spent_avg = sum(retail_visited_tiers)/len(retail_visited_tiers)
+    try:
+        services_spent_avg = sum(services_used_tiers)/len(services_used_tiers)
+    except:
+        pass
+
+    coffee_rec_avg = sum(coffee_recs_tiers)/len(coffee_recs_tiers)
+    food_rec_avg = sum(food_recs_tiers)/len(food_recs_tiers)
+    bars_rec_avg = sum(bars_recs_tiers)/len(bars_recs_tiers)
+    retail_rec_avg = sum(retail_recs_tiers)/len(retail_recs_tiers)
+    try:
+        services_rec_avg = sum(services_recs_tiers)/len(services_recs_tiers)
+    except:
+        pass
+
+    total_food = sum(food.values())
+    total_coffee = sum(coffee.values())
+    total_bars = sum(bars.values())
+    total_services = sum(services.values())
+    total_retail = sum(retail.values())
+
+    coffee_savings_avg = (((coffee_spent_avg - coffee_rec_avg) * (0.4)) * total_coffee)/4
+    food_savings_avg = (((food_spent_avg - food_rec_avg) * (0.4)) * total_food)/4
+    bars_savings_avg = (((bars_spent_avg - bars_rec_avg) * (0.4)) * total_bars)/4
+    retail_savings_avg = (((retail_spent_avg - retail_rec_avg) * (0.4)) * total_retail)/4
+    try:
+        services_savings_avg = (((services_spent_avg - services_rec_avg) * (0.4)) * total_services)/4
+    except:
+        pass
+
+    script, div, food_monthly, services_monthly, bars_monthly, coffee_monthly, retail_monthly = spendings_plot(spendings_dict, df)
+
+    recs_list = []
+
+    for key, value in high_rates_recs.items():
+        for rec in value:
+            if rec not in recs_list:
+                recs_list.append(rec)
+
+    return render_template("recspage.html", cheapdict=cheap_recs, best_rating=recs_list,rec_breakdown=rec_breakdown,
+                            spendings=spendings_dict, food=food, coffee=coffee,bars=bars, services=services, retail=retail,
+                            script=script, div=div, food_monthly=food_monthly, services_monthly=services_monthly,
+                            bars_monthly=bars_monthly, coffee_monthly=coffee_monthly, retail_monthly=retail_monthly,
+                            averagecoffee=coffee_savings_avg, averagefood=food_savings_avg, averagebars=bars_savings_avg,
+                            averageretail=retail_savings_avg)
+
+@app.route('/recs/business', methods=["POST", "GET"])
+def business_recs():
+
+    num_matrix = pd.read_pickle("./num_matrix.pkl")
+    df = pd.read_pickle('./final_df.pkl')
+
+    business = request.args.get('name')
+    matches = df[df['business'].str.contains(business, case=False)]
+
+    matches = matches.reset_index()
+    business_match = matches['business'][0]
+
+    business_categories = []
+
+    for col in matches.columns:
+        if matches[col][0] == 1:
+            business_categories.append(col)
+
+    categories =  ",".join(business_categories)
+
+    all_recs, cheap_recs, best_recs = best_business_recs(business_match, num_matrix, df)
+
+
+    return render_template("businessrecspage.html", business_match=business_match, cheapdict=cheap_recs,
+                            best_rating_dict=best_recs, price=matches['price'][0], categories=categories,
+                            reviews=matches['review_count'][0], rating=matches['rating'][0], link=matches['url'][0],
+                            pic=matches['image_url'][0])
+
+
+    if __name__ == '__main__':
+        app.run()
